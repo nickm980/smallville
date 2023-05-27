8f4aefe82c4395e19bf95bbde326ae4aebec1e30
nickm980 <nicholasjmattern@gmail.com>
2023-05-27 00:21:34 -0400

    wip: created a new update filter

---
+++ smallville/src/main/java/io/github/nickm980/smallville/update/UpdateProgress.java
@@ -0,0 +1,19 @@
+package io.github.nickm980.smallville.update;
+
+import io.github.nickm980.smallville.World;
+import io.github.nickm980.smallville.entities.Agent;
+
+public class UpdateProgress extends AgentUpdate {
+
+    private final Progress progress;
+
+    public UpdateProgress(Progress progress) {
+	this.progress = progress;
+    }
+
+    @Override
+    protected boolean update(IChatService converter, World world, Agent agent) {
+	progress.update();
+	return true;
+    }
+}
