7023bd3fe2711463d361b92a5f08928a1345c741
nickm980 <nicholasjmattern@gmail.com>
2023-06-02 11:41:13 -0400

    observations will act as reactions and attempt updating plans

--- smallville/src/main/java/io/github/nickm980/smallville/update/UpdateFuturePlans.java
+++ smallville/src/main/java/io/github/nickm980/smallville/update/UpdateFuturePlans.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import io.github.nickm980.smallville.World;
 import io.github.nickm980.smallville.entities.Agent;
+import io.github.nickm980.smallville.entities.memory.Observation;
 import io.github.nickm980.smallville.entities.memory.Plan;
 import io.github.nickm980.smallville.entities.memory.PlanType;
 
@@ -12,17 +13,24 @@ public class UpdateFuturePlans extends AgentUpdate {
     @Override
     public boolean update(IChatService converter, World world, Agent agent) {
 	LOG.info("[Plans] Updating plans");
-	List<Plan> memories = agent.getMemoryStream().prunePlans();
+	boolean hasPlans = !agent.getMemoryStream().getPlans().isEmpty();
+	boolean shouldUpdatePlans = !hasPlans;
 
-	LOG.info("[Plans] Pruned " + memories.size() + " old plans");
+	Observation observation = agent.getMemoryStream().getLastObservation();
 
-	agent.getMemoryStream().add(converter.convertFuturePlansToMemories(memories));
+	if (observation != null && observation.isReactable()) {
+	    shouldUpdatePlans = converter.shouldUpdatePlans(agent, observation.getDescription());
+	    if (shouldUpdatePlans) {
+		LOG.info("[Plans] Changing plans...");
+	    } else {
+		LOG.info("[Plans] Not changing plans...");
+	    }
+	}
 
-	if (agent.getPlans().isEmpty() || agent.getPlans().size() < 3) {
-	    LOG.info("[Plans] Updating long term plans");
-
+	// initialize the agent plans if none are found
+	if (shouldUpdatePlans) {
 	    List<Plan> plans = converter.getPlans(agent);
-	    agent.addPlans(plans);
+	    agent.getMemoryStream().addAll(plans);
 
 	    for (Plan plan : plans) {
 		LOG.info("[Plans] " + plan.getType() + " " + plan.asNaturalLanguage());
@@ -31,19 +39,17 @@ public class UpdateFuturePlans extends AgentUpdate {
 	    LOG.info("[Plans] Updated long term plans");
 	}
 
-	if (agent.getMemoryStream().getPlans(PlanType.SHORT_TERM).isEmpty()) {
-	    LOG.info("[Plans] Updating short term plans");
-
+	if (agent.getMemoryStream().getPlans(PlanType.SHORT_TERM).isEmpty() || shouldUpdatePlans) {
 	    List<Plan> plans = converter.getShortTermPlans(agent);
 
 	    for (Plan plan : plans) {
-		plan.convertToShortTermMemory(true, PlanType.SHORT_TERM);
+		plan.convert(PlanType.SHORT_TERM);
 		LOG.info("[Plans] " + plan.getType() + " " + plan.asNaturalLanguage());
 	    }
 
 	    agent.getMemoryStream().setShortTermPlans(plans);
 
-	    LOG.info("[Plans] Recursively updated short term plans");
+	    LOG.info("[Plans] Updated short term plans");
 	}
 
 	LOG.info("[Plans] Plans updated");
