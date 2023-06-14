4e1888b95c03cbd4217ac033d38fc7e2b9992720
nickm980 <nicholasjmattern@gmail.com>
2023-06-14 18:22:17 -0400

    wip: finished basic support for function calling

--- smallville/src/main/java/io/github/nickm980/smallville/config/SmallvilleConfig.java
+++ smallville/src/main/java/io/github/nickm980/smallville/config/SmallvilleConfig.java
@@ -10,11 +10,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.github.nickm980.smallville.config.prompts.Prompts;
 
 public class SmallvilleConfig {
     private static Prompts prompts;
     private static GeneralConfig config;
+    private static JsonNode functions;
+
     private static final Logger LOG = LoggerFactory.getLogger(SmallvilleConfig.class);
 
     private SmallvilleConfig() {
@@ -23,8 +28,8 @@ public class SmallvilleConfig {
 
     public static Prompts getPrompts() {
 	if (prompts == null) {
-	    LOG.info("Loading prompts");
-	    prompts = loadFile("prompts.yaml", Prompts.class);
+	    LOG.info("Loading prompts...");
+	    prompts = loadYamlFile("prompts.yaml", Prompts.class);
 	}
 
 	return prompts;
@@ -32,23 +37,31 @@ public class SmallvilleConfig {
 
     public static GeneralConfig getConfig() {
 	if (config == null) {
-	    LOG.info("Loading config.yaml");
-	    config = loadFile("config.yaml", GeneralConfig.class);
+	    LOG.info("Loading config.yaml...");
+	    config = loadYamlFile("config.yaml", GeneralConfig.class);
 	}
 
 	return config;
     }
 
-    private static <T> T loadFile(String file, Class<T> clazz) {
-	Yaml yaml = new Yaml();
+    public static JsonNode getFunctions() {
+	if (functions == null) {
+	    LOG.info("Loading functions json...");
+	    functions = loadJsonFile("functions.json");
+	}
 
-	Path configFile = Paths.get(file);
+	return functions;
+    }
+
+    private static InputStream loadInputStream(String path) {
+	Path file = Paths.get(path);
+
 	InputStream inputStream = null;
 
-	if (Files.exists(configFile)) {
+	if (Files.exists(file)) {
 	    LOG.debug("Configuration file found");
 	    try {
-		inputStream = Files.newInputStream(configFile);
+		inputStream = Files.newInputStream(file);
 	    } catch (IOException e) {
 		e.printStackTrace();
 	    }
@@ -61,6 +74,35 @@ public class SmallvilleConfig {
 	    LOG.error("No " + file + " found. It must be either in resources folder or next to jar");
 	}
 
-	return yaml.loadAs(inputStream, clazz);
+	return inputStream;
+    }
+
+    private static JsonNode loadJsonFile(String file) {
+	InputStream stream = loadInputStream(file);
+	ObjectMapper mapper = new ObjectMapper();
+	JsonNode result = null;
+	
+	try {
+	    result = mapper.readTree(stream);
+	    stream.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	return result;
+    }
+
+    private static <T> T loadYamlFile(String file, Class<T> clazz) {
+	Yaml yaml = new Yaml();
+	InputStream stream = loadInputStream(file);
+	T result = yaml.loadAs(stream, clazz);
+
+	try {
+	    stream.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	return result;
     }
 }
