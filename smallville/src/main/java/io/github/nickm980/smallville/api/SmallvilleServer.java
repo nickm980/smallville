2172629ff7c31490d8a842686e016cf0bd8dfbc4
nickm980 <nicholasjmattern@gmail.com>
2023-06-22 13:56:32 -0400

    added endpoint versioning support

--- smallville/src/main/java/io/github/nickm980/smallville/api/SmallvilleServer.java
+++ smallville/src/main/java/io/github/nickm980/smallville/api/SmallvilleServer.java
@@ -1,37 +1,21 @@
 package io.github.nickm980.smallville.api;
 
-import java.io.StringWriter;
-import java.time.Duration;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalUnit;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 
-import io.github.nickm980.smallville.LogCache;
 import io.github.nickm980.smallville.World;
-import io.github.nickm980.smallville.api.dto.*;
-import io.github.nickm980.smallville.entities.SimulationTime;
+import io.github.nickm980.smallville.api.v1.EndpointsV1;
 import io.github.nickm980.smallville.llm.LLM;
 import io.javalin.Javalin;
 
 public class SmallvilleServer {
 
     private final SimulationService service;
-    private MustacheFactory mf = new DefaultMustacheFactory();
+    private MustacheFactory mf;
 
     public SmallvilleServer(LLM llm, World sim) {
 	this.service = new SimulationService(llm, sim);
-    }
-
-    public SimulationService getSimulationService() {
-	return service;
+	this.mf = new DefaultMustacheFactory();
     }
 
     public void start() {
@@ -41,7 +25,6 @@ public class SmallvilleServer {
     public void start(int port) {
 	Javalin app = Javalin.create(config -> {
 	    config.showJavalinBanner = false;
-	    config.staticFiles.enableWebjars();
 	    config.staticFiles.add("./");
 	    config.plugins.enableCors(cors -> {
 		cors.add(it -> {
@@ -50,172 +33,13 @@ public class SmallvilleServer {
 	    });
 	});
 
+	EndpointsV1.register(service, mf, app);
 	ExceptionRoutes.registerExceptions(app);
-
-	app.get("/dashboard", (ctx) -> {
-	    Map<String, Object> model = new HashMap<>();
-	    model.put("agents", service.getAgents());
-	    model.put("objects", service.getChangedLocations());
-	    Mustache mustache = mf.compile("index.mustache");
-	    String output = mustache.execute(new StringWriter(), model).toString();
-	    ctx.html(output);
-	});
-
-	app.get("/dashboard/{name}", (ctx) -> {
-	    Map<String, Object> model = new HashMap<>();
-	    model.put("memories", service.getMemories(ctx.pathParam("name")));
-
-	    Mustache mustache = mf.compile("agent.mustache");
-	    String output = mustache.execute(new StringWriter(), model).toString();
-	    ctx.html(output);
-	});
-
-	app.get("/", (ctx) -> {
-	    ctx.redirect("/dashboard");
-	});
-
-	app.get("/info", (ctx) -> {
-	    String time = SimulationTime.now().format(DateTimeFormatter.ofPattern("h:mm a"));
-
-	    ctx
-		.json(Map
-		    .of("time", time, "step", (int) SimulationTime.getStepDuration().getSeconds() / 60, "prompts",
-			    LogCache.getPrompts()));
-	});
-
-	app.get("/ping", (ctx) -> {
-	    ctx.result("pong");
-	});
-
-	app.get("/agents", (ctx) -> {
-	    ctx.json(Map.of("agents", service.getAgents()));
-	});
-
-	app.get("/agents/{name}", (ctx) -> {
-	    AgentStateResponse res = service.getAgentState(ctx.pathParam("name"));
-	    ctx.json(res);
-	});
-
-	app.post("/agents/{name}/ask", (ctx) -> {
-	    AskQuestionRequest request = ctx
-		.bodyValidator(AskQuestionRequest.class)
-		.check((req) -> exists(req.getQuestion()), "{question} cannot be blank")
-		.get();
-
-	    String res = service.askQuestion(ctx.pathParam("name"), request.getQuestion());
-
-	    ctx.json(Map.of("answer", res));
-	});
-
-	app.post("/agents/{name}/goal", (ctx) -> {
-	    SetGoalRequest request = ctx
-		.bodyValidator(SetGoalRequest.class)
-		.check((req) -> exists(req.getGoal()), "{goal} cannot be blank")
-		.get();
-
-	    service.setGoal(ctx.pathParam("name"), request.getGoal());
-
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.post("/agents", (ctx) -> {
-	    CreateAgentRequest request = ctx
-		.bodyValidator(CreateAgentRequest.class)
-		.check((req) -> exists(req.getName()), "{name} cannot be missing")
-		.check((req) -> exists(req.getActivity()), "{activity} cannot be missing")
-		.check((req) -> exists(req.getLocation()), "{location} cannot be missing")
-		.check((req) -> req.getMemories() != null && !req.getMemories().isEmpty(),
-			"{memories} cannot be missing")
-		.get();
-
-	    service.createPerson(request);
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.post("/locations", (ctx) -> {
-	    CreateLocationRequest request = ctx
-		.bodyValidator(CreateLocationRequest.class)
-		.check((req) -> exists(req.getName()), "{name} cannot be missing")
-		.get();
-
-	    service.createLocation(request);
-	    ctx.json(Map.of("success", true));
-	});
-
-	class LocationState {
-	    String state;
-	}
-
-	app.post("/locations/{name}", (ctx) -> {
-	    String location = ctx.pathParam("name");
-	    ObjectMapper objectMapper = new ObjectMapper();
-
-	    JsonNode rootNode = objectMapper.readTree(ctx.body());
-	    String state = rootNode.get("state").asText();
-
-	    service.setState(location, state);
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.post("/objects", (ctx) -> {
-	    CreateObjectRequest request = ctx
-		.bodyValidator(CreateObjectRequest.class)
-		.check((req) -> exists(req.getName()), "{name} cannot be missing")
-		.check((req) -> exists(req.getParent()), "{parent} location cannot be missing")
-		.check((req) -> exists(req.getState()), "{state} cannot be missing")
-		.get();
-
-	    service.createObject(request);
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.get("/locations", (ctx) -> {
-	    List<LocationStateResponse> request = service.getChangedLocations();
-
-	    ctx.json(Map.of("locations", request));
-	});
-
-	app.post("/memories", (ctx) -> {
-	    CreateMemoryRequest request = ctx.bodyAsClass(CreateMemoryRequest.class);
-	    service.createMemory(request);
-
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.post("/state", (ctx) -> {
-	    service.updateState();
-	    List<AgentStateResponse> agents = service.getAgents();
-	    List<LocationStateResponse> locations = service.getChangedLocations();
-	    List<ConversationResponse> conversations = service.getConversations();
-
-	    ctx.json(Map.of("agents", agents, "location_states", locations, "conversations", conversations));
-	});
-
-	app.get("/state", (ctx) -> {
-	    List<AgentStateResponse> agents = service.getAgents();
-	    List<LocationStateResponse> locations = service.getChangedLocations();
-	    List<ConversationResponse> conversations = service.getConversations();
-
-	    ctx.json(Map.of("agents", agents, "location_states", locations, "conversations", conversations));
-	});
-
-	// Set how many minutes each state update will increment time by
-	app.post("/timestep", (ctx) -> {
-	    SetTimestepRequest request = ctx.bodyAsClass(SetTimestepRequest.class);
-	    int minutes = Integer.valueOf(request.getNumOfMinutes());
-	    SimulationTime.setStep(Duration.ofMinutes(minutes));
-	    ctx.json(Map.of("success", true));
-	});
-
-	app.get("/progress", (ctx) -> {
-	    int progress = service.getProgress();
-	    ctx.json(Map.of("max", 100, "current", progress));
-	});
 
 	app.start(port);
     }
 
-    private boolean exists(String s) {
+    public static boolean exists(String s) {
 	return s != null && !s.isBlank();
     }
 }
