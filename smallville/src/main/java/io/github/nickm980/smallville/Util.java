37ec7a753d205fa71416bb9d7d200607173cc7b8
nickm980 <nicholasjmattern@gmail.com>
2023-06-22 13:48:46 -0400

    Creating parsing methods for chat responses

--- smallville/src/main/java/io/github/nickm980/smallville/Util.java
+++ smallville/src/main/java/io/github/nickm980/smallville/Util.java
@@ -1,12 +1,46 @@
 package io.github.nickm980.smallville;
 
-public class Util {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-    public static String[] parseLocation(String fullName) {
-	String[] locations = fullName.split(":");
+import com.google.gson.Gson;
+
+public class Util {
+    private static final Gson gson = new Gson();
+
+    public static String[] parseLocation(String location) {
+	Objects.requireNonNull(location);
+	String[] locations = location.split(":");
 	String[] result = new String[2];
+	result[0] = " ";
+	result[1] = " ";
+
 	result[0] = locations[0].trim();
-	result[1] = locations[1].trim();
+
+	if (locations.length > 1) {
+	    result[1] = locations[1].trim();
+	}
+
+	return result;
+    }
+
+    public static <T> T parseAsClass(String input, Class<T> clazz) {
+	Map<String, String> data = parseCson(input);
+
+	return gson.fromJson(gson.toJson(data), clazz);
+    }
+
+    public static Map<String, String> parseCson(String input) {
+	HashMap<String, String> result = new HashMap<String, String>();
+
+	for (String line : input.split("\n")) {
+	    if (line.contains(":")) {
+		String[] values = line.split(":", 2);
+		result.put(values[0].toLowerCase(), values[1].trim());
+	    }
+	}
+
 	return result;
     }
 }
