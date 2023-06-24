566647ba727f5d8ec41cc17a03d9b381d52f04ec
nickm980 <nicholasjmattern@gmail.com>
2023-06-23 21:37:18 -0400

    conversation update bug fixes

--- smallville/src/main/java/io/github/nickm980/smallville/update/UpdateReactionAndFuturePlans.java
+++ smallville/src/main/java/io/github/nickm980/smallville/update/UpdateReactionAndFuturePlans.java
@@ -10,6 +10,7 @@ import io.github.nickm980.smallville.entities.memory.Plan;
 import io.github.nickm980.smallville.entities.memory.PlanType;
 import io.github.nickm980.smallville.nlp.LocalNLP;
 import io.github.nickm980.smallville.nlp.NLPCoreUtils;
+import io.github.nickm980.smallville.prompts.ChatService;
 import io.github.nickm980.smallville.prompts.Prompts;
 import io.github.nickm980.smallville.prompts.dto.Reaction;
 
@@ -19,55 +20,23 @@ import io.github.nickm980.smallville.prompts.dto.Reaction;
  */
 public class UpdateReactionAndFuturePlans extends AgentUpdate {
     private static final NLPCoreUtils TOKENIZER = new LocalNLP();
-    
+
     @Override
     public boolean update(Prompts converter, World world, Agent agent) {
 	LOG.info("[Plans] Updating plans...");
 	boolean hasPlans = !agent.getMemoryStream().getPlans().isEmpty();
 	boolean shouldUpdatePlans = !hasPlans;
-
+	boolean shouldUpdateConversation = false;
+	String conversationMessage = null;
+	
 	Observation observation = agent.getMemoryStream().getLastObservation();
 
 	if (observation.isReactable()) {
+	    LOG.info("starting reaction to an observation");
 	    Reaction reaction = converter.shouldUpdatePlans(agent, observation.getDescription());
 	    shouldUpdatePlans = reaction.getAnswer().toLowerCase().contains("yes");
-	    boolean shouldUpdateConversation = reaction.getConversation().toLowerCase().contains("yes");
-
-	    if (shouldUpdateConversation) {
-		String subject = TOKENIZER.extractLastOccurenceOfName(observation.getDescription());
-
-		if (agent.getFullName().equals(subject)) {
-		    LOG.warn("[Conversation] Agent attempted to have a conversation with themself.");
-		    return false;
-		}
-
-		if (subject == null) {
-		    LOG.info("[Conversation] No conversation detected");
-		    return true;
-		}
-
-		Agent other = world.getAgent(subject).orElse(null);
-
-		if (other == null) {
-		    LOG
-			.error("[Conversation] A conversation was attempted but the target {" + subject
-				+ "} does not exist.");
-		    return false;
-		}
-
-		Conversation conversation = converter.getConversationIfExists(agent, other, observation.getDescription());
-
-		List<Observation> memories = conversation
-		    .getDialog()
-		    .stream()
-		    .map(dialog -> new Observation(dialog.asNaturalLanguage()))
-		    .toList();
-
-		agent.getMemoryStream().addAll(memories);
-		other.getMemoryStream().addAll(memories);
-
-		world.create(conversation);
-	    }
+	    conversationMessage = reaction.getConversation();
+	    shouldUpdateConversation = reaction.getConversation().toLowerCase().contains("yes");
 	}
 
 	if (shouldUpdatePlans) {
@@ -82,9 +51,48 @@ public class UpdateReactionAndFuturePlans extends AgentUpdate {
 	    updatePlans(converter, agent, PlanType.SHORT_TERM);
 	}
 
+	if (shouldUpdateConversation) {
+	    updateConversation(agent, converter, world, observation.getDescription());
+	}
+	
 	LOG.info("[Plans] Plans updated");
 
 	return next(converter, world, agent);
+    }
+
+    private boolean updateConversation(Agent agent, Prompts converter, World world, String observation) {
+	String subject = TOKENIZER.extractLastOccurenceOfName(observation);
+
+	if (agent.getFullName().equals(subject)) {
+	    LOG.warn("[Conversation] Agent attempted to have a conversation with themself.");
+	    return false;
+	}
+
+	if (subject == null) {
+	    LOG.warn("[Conversation] Conversation detected but no subject found");
+	    return true;
+	}
+
+	Agent other = world.getAgent(subject).orElse(null);
+
+	if (other == null) {
+	    LOG.error("[Conversation] A conversation was attempted but the target {" + subject + "} does not exist.");
+	    return false;
+	}
+
+	Conversation conversation = converter.getConversationIfExists(agent, other, observation);
+
+	List<Observation> memories = conversation
+	    .getDialog()
+	    .stream()
+	    .map(dialog -> new Observation(dialog.asNaturalLanguage()))
+	    .toList();
+
+	agent.getMemoryStream().addAll(memories);
+	other.getMemoryStream().addAll(memories);
+
+	world.create(conversation);
+	return false;
     }
 
     private void updatePlans(Prompts converter, Agent agent, PlanType type) {
