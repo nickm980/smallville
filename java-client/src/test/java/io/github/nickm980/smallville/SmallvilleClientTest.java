411cfd40eb2e0b0e3c09128eb555dab76b782548
nickm980 <nicholasjmattern@gmail.com>
2023-07-21 17:50:35 -0400

    removed createObject method
    Replaced createObject method with createLocation, which takes in locations separated by colons and an optional state

--- java-client/src/test/java/io/github/nickm980/smallville/SmallvilleClientTest.java
+++ java-client/src/test/java/io/github/nickm980/smallville/SmallvilleClientTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -32,41 +33,37 @@ public class SmallvilleClientTest {
     @Test
     @Order(1)
     public void testLocationCreation() {
-	assertTrue(client.createLocation("Yellow House"));
-	assertTrue(client.createObject("Yellow House", "Ball", new ObjectState("deflated")));
+	assertTrue(client.createLocation("Yellow House: Bedroom"));
     }
 
     @Test
     @Order(2)
     public void testAgentCreation() {
-	List<String> memories = List.of("memory 1", "memory 2");
-	boolean success = client.createAgent("Joe", memories, "Yellow House: Ball", "cooking food");
+	List<String> memories = List.of("Joe loves to play video games", "Joe lives in the yellow house");
+	boolean success = client.createAgent("Joe", memories, "Yellow House: Bedroom", "sleeping");
 
 	assertEquals(true, success);
     }
 
     @Test
     @Order(3)
+    @Disabled
     public void testAskQuestion() {
 	String response = client.ask("Joe", "what is your name?");
 	System.out.println(response);
 	assertNotNull(response);
     }
 
-    @Test
-    @Order(4)
-    public void testAddObservationWithoutReacting() {
-	assertTrue(client.addObservation("Joe", "The house is on fire", false));
-    }
 
     @Test
-    @Order(5)
+    @Order(4)
     public void testReactToObservation() {
-	assertTrue(client.addObservation("Joe", "The house is on fire", true));
+	assertTrue(client.addObservation("Joe", "The yellow house is on fire", true));
     }
 
     @Test
     @Order(6)
+    @Disabled
     public void testGameUpdates() {
 	client.updateState();
     }
