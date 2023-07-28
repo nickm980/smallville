8ca91cfd6d10973c376a7dde4fb6dfa45cf1463b
nickm980 <nicholasjmattern@gmail.com>
2023-07-28 06:37:00 -0400

    updated NODE_ENV variable

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
-
+console.log(isProd)
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
